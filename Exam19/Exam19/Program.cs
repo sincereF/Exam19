1755f7eb027a51a50145070b83aa916375592646
Daniil Sardov <77523787+sincereF@users.noreply.github.com>
2021-06-05 15:30:26 +0400

    Exam19
    Exam var19

---
+++ Exam19/Exam19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exam19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Написать программу для подсчета суммы чисел, кратных 3 в диапазоне от 30 до 60:");
+            
+
+            counting count = new counting();
+            for (int i = 30; i <= 60; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count.Sum = i;
+                }
+            }
+
+            Console.WriteLine("\nСумма чисел равна " + count.Sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exam19/Exam19/counting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam19
+{
+    class counting
+    {
+        public int sum = 0;
+
+        public int Sum
+        {
+            set
+            {
+                sum += value;
+            }
+            get
+            {
+                return sum;
+            }
+        }
+    }
+}
